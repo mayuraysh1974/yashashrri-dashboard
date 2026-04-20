41f6a0c5f3d48eb492ae655a5cb8cd8bdc8eb6a6
Mayuraysh <mayuraysh1974@gmail.com>
2026-04-20 14:46:51 +0530

    Fix: Added missing studentResults state to TestScheduler to enable mark recording

--- src/pages/TestScheduler.jsx
+++ src/pages/TestScheduler.jsx
@@ -19,6 +19,7 @@ const TestScheduler = () => {
   const [uploading, setUploading] = useState(false);
   const [editMode, setEditMode] = useState(false);
   const [activeTestId, setActiveTestId] = useState(null);
+  const [studentResults, setStudentResults] = useState([]);
 
   useEffect(() => {
     fetchTests();
